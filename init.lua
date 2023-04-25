7baa1b0f55d1bf7faf5dae16936d8e6eff45fa59
Faouzi1406 <87326@glr.nl>
2023-04-25 16:09:06 +0200

    New colorscheme

--- init.lua
+++ init.lua
@@ -43,8 +43,8 @@ require('lazy').setup({
 
   -- The c# experience babyyyy
   'OmniSharp/omnisharp-vim',
-  'marko-cerovac/material.nvim',
-
+  -- cappuccin color scheme scheme
+  { "catppuccin/nvim", name = "catppuccin" },
   'tpope/vim-rhubarb',
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
@@ -562,66 +562,6 @@ cmp.setup {
   },
 }
 
-vim.g.material_style = "darker"
-
-vim.cmd 'colorscheme material'
-
-require('material').setup({
-  contrast = {
-    terminal = false,            -- Enable contrast for the built-in terminal
-    sidebars = false,            -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
-    floating_windows = false,    -- Enable contrast for floating windows
-    cursor_line = false,         -- Enable darker background for the cursor line
-    non_current_windows = false, -- Enable darker background for non-current windows
-    filetypes = {},              -- Specify which filetypes get the contrasted (darker) background
-  },
-  styles = {
-    -- Give comments style such as bold, italic, underline etc.
-    comments = { --[[ italic = true ]] },
-    strings = { --[[ bold = true ]] },
-    keywords = { --[[ underline = true ]] },
-    functions = { --[[ bold = true, undercurl = true ]] },
-    variables = {},
-    operators = {},
-    types = {},
-  },
-  plugins = { -- Uncomment the plugins that you use to highlight them
-    -- Available plugins:
-    -- "dap",
-    -- "dashboard",
-    -- "gitsigns",
-    -- "hop",
-    -- "indent-blankline",
-    -- "lspsaga",
-    -- "mini",
-    -- "neogit",
-    -- "neorg",
-    -- "nvim-cmp",
-    -- "nvim-navic",
-    -- "nvim-tree",
-    -- "nvim-web-devicons",
-    -- "sneak",
-    -- "telescope",
-    -- "trouble",
-    -- "which-key",
-  },
-  disable = {
-    colored_cursor = false, -- Disable the colored cursor
-    borders = false,        -- Disable borders between verticaly split windows
-    background = false,     -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
-    term_colors = false,    -- Prevent the theme from setting terminal colors
-    eob_lines = false       -- Hide the end-of-buffer lines
-  },
-  high_visibility = {
-    lighter = false,         -- Enable higher contrast text for lighter style
-    darker = false           -- Enable higher contrast text for darker style
-  },
-  lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
-  async_loading = true,      -- Load parts of the theme asyncronously for faster startup (turned on by default)
-  custom_colors = nil,       -- If you want to everride the default colors, set this to a function
-  custom_highlights = {},    -- Overwrite highlights with your own
-})
-
 local opts = {
   tools = {
     -- rust-tools options
@@ -829,3 +769,5 @@ require('copilot').setup({
   copilot_node_command = 'node', -- Node.js version must be > 16.x
   server_opts_overrides = {},
 })
+
+require "colerscheme"
