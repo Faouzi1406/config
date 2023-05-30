ac90657bd17a8716b4c0c39db7e70fd8a2f4fd10
Faouzi1406 <87236@glr.nl>
2023-05-30 23:08:33 +0200

    fix: Fix luasnip :(

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -14,7 +14,7 @@ return {
 	{
 		"L3MON4D3/LuaSnip",
 		-- follow latest release.
-		version = "1.2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		version = "1.*", -- Replace <:tf:> by the latest released major (first number of latest release)
 		-- install jsregexp (optional!).
 		build = "make install_jsregexp"
 	},
